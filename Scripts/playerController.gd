d852763a8cc1a51f82bcd64aab7cfd0b686a437a
Biskkit <abdulbasitsalim921@gmail.com>
2024-06-21 16:17:24 -0400

    Attached "playerController" script to original player scene

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
@@ -12,7 +11,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
